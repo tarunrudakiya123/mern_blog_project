b2046c78add82f540e1d2b189ebc77734f312340
Your Name <you@example.com>
2024-02-11 22:41:04 +0530

    Connect MongoDb to server

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import env from 'dotenv';
+
+env.config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log("MongpDb is connted.....")
+}).catch((error) => {
+    console.log(error)
+})
 
 const app = express();
 
 
-app.listen(3000,()=>{
-    console.log("Server started at 3000")
+app.listen(3000, () => {
+    console.log("Server started at 3000....")
 })
