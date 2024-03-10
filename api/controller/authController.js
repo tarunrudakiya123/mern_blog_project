8e3488b29e3078e721192a4dabaeba78b177411a
Your Name <you@example.com>
2024-03-11 00:06:28 +0530

    Create signIn api

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
-
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -26,3 +26,42 @@ export const signup = async (req, res, next) => {
 
 }
 
+export const signIn = async (req, res, next) => {
+
+    const { email, password } = req.body
+
+    if (!email || !password ) {
+        next(errorHandler(400, "All feilds are required"))
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+           return next(errorHandler(400, 'Invalid password'))
+        }
+
+        const token = jwt.sign(
+            { _id: validUser._id }, process.env.JET_SECRET);
+
+        const { password: pass, ...rest} = validUser._doc;
+
+
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest)
+
+
+    } catch (error) {
+        next(error)
+    }
+
+
+}
--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signup } from "../controller/authController.js";
+import { signIn, signup } from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signIn);
+
 
 export default router;
