0a6bc51cb9688736d14f11dac81e5525f6610c6a
Your Name <you@example.com>
2024-03-13 23:32:09 +0530

    add redux persist

---
+++ client/src/redux/store.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from '../redux/user/userSlice'; 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import themeReducer from '../redux/theme/themeSlice';
+
+const rootReducer = combineReducers({
+  user: userReducer, 
+  theme: themeReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
